d6c6f76807f171775ff8455f1902056e0d968aea
Daman Bual <damanbual@gmail.com>
2024-06-10 12:18:55 -0700

    finished exercise

--- timer1.js
+++ timer1.js
@@ -1,8 +1,15 @@
 
 const args = process.argv.slice(2);
 
-// Filter out invalid inputs (non-numeric or negative numbers)
+// Filtered out invalid inputs 
 const times = args.filter(arg => {
   const num = Number(arg);
   return !isNaN(num) && num > 0;
-}).map(Number);
+}).map(Number); 
+
+// Scheduled a beep for each valid input time
+times.forEach(time => {
+  setTimeout(() => {
+    process.stdout.write("\x07");
+  }, time * 1000); 
+});
